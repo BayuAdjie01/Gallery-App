397993433f38507df0482a22841e78b5cf9577cd
Bayu Seno Adjie <bayuadjie111@gmail.com>
2023-06-24 23:45:53 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Bayu Seno Adjie" // TODO: replace this
+const studentId = "FE5274680" // TODO: replace this
+
+export { url, studentName, studentId }
